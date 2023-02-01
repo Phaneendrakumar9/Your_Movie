5f9c560e62ad1158aabacbee861d156428518830
Phaneendrakumar9 <phaneendra@sigaramtech.com>
2023-02-02 01:01:39 +0530

    Added progress Bar in Login Screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/useAuth'
 import {RecoilRoot} from 'recoil'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <RecoilRoot>
       <AuthProvider>
+        <NextNProgress color='red'/>
            <Component {...pageProps} />
         </AuthProvider> 
     </RecoilRoot>
